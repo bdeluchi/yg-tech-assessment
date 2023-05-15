ef705d207835dafeb5ff883a421bd634850b75d4
bdeluchi <deluchibso@gmail.com>
2023-05-15 17:20:09 +0200

    add test case for multiple repos

--- src/repo-list/repo-list-page.test.tsx
+++ src/repo-list/repo-list-page.test.tsx
@@ -3,7 +3,7 @@ import {RepoListPage} from "./repo-list-page";
 import {RepoItem} from "./repo-item";
 
 const getAll = jest.fn();
-it("displays a repo with all relevant fields", async () => {
+it("displays fetched repo with all relevant fields", async () => {
     const repo: RepoItem = {id: 1, name: 'React', forks: 1, stars: 2, url: 'random.org'}
 
     getAll.mockResolvedValue([repo])
@@ -21,5 +21,16 @@ it("repo name contains external url", async () => {
     expect(await screen.findByRole('link', {name: /React/i})).toHaveAttribute('href', 'random.org')
 });
 
-it.todo("displays a list of repos");
+it("displays fetched list of multiple repos", async () => {
+    const repo: RepoItem = {id: 1, name: 'React', forks: 1, stars: 2, url: 'random.org'}
+    const additionalRepo: RepoItem = {id: 2, name: 'Remix', forks: 23, stars: 10, url: 'remix.com'}
+
+    getAll.mockResolvedValue([repo, additionalRepo])
+    render(<RepoListPage repository={{getAll}}/>)
+    expect(await screen.findByText(/React/i)).toBeInTheDocument();
+    expect(screen.getByText(/Remix/i)).toBeInTheDocument();
+});
+
+it.todo("shows loading status")
+it.todo("shows error message in case of any error")
 
